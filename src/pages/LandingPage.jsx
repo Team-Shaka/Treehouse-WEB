d2ec5f3d8d750856e035a1508d717f1739cdc124
Dawon00 <dawon000203@gmail.com>
2024-07-15 16:27:19 +0900

    chore: Update font family

---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function LandingPage() {
+  return (
+    <div class="text-center lg:text-left p-4 lg:p-8">
+      <h1 class="text-head1 lg:text-head2">반응형 텍스트</h1>
+      <p class="text-body1 lg:text-body2">이 문장은 반응형입니다.</p>
+    </div>
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,39 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Pretendard", "sans-serif"],
+      },
+      colors: {
+        tree_green: "#1AA873",
+        gray: {
+          1: "#F4F4F4",
+          2: "#EDEDED",
+          3: "#DEDEDE",
+          4: "#CCCCCC",
+          5: "#AEAEAE",
+          6: "#8D8D8D",
+          7: "#6B6B6B",
+          8: "#4A4A4A",
+          9: "#2D2D2D",
+          black: "#0E0E0E",
+          white: "#FDFDFD",
+        },
+      },
+      fontSize: {
+        head1: ["44px", "60px"],
+        head2: ["28px", "50px"],
+        body1: ["24px", "40px"],
+        caption1: ["18px", "26px"],
+      },
+      fontWeight: {
+        head1: "bold", // head_44_B
+        head2: "semibold", // head_28_SB
+        body1: "medium", // body_24_M
+        caption1: "medium", // caption_18_M
+      },
+    },
   },
   plugins: [],
 };
