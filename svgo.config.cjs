170895c53624f35c2e75902bc888c79d9c406117
Dawon00 <dawon000203@gmail.com>
2024-07-23 16:11:26 +0900

    chore: Add necessary plugins and configurations for SVG optimization

--- svgo.config.cjs
+++ svgo.config.cjs
@@ -1,8 +1,9 @@
 // eslint-disable-next-line no-undef
 module.exports = {
   plugins: [
-    { name: "removeViewBox", active: false },
+    { name: "removeViewBox", active: false }, // viewBox 제거 비활성화
     { name: "removeDimensions", active: true },
     { name: "convertPathData", active: true },
+    // 필요한 플러그인 추가 및 설정
   ],
 };
