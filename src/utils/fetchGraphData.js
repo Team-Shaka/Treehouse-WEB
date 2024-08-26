0fc7b0f9e58cf97b36ee7b76a953b8e439aea917
Dawon00 <dawon000203@gmail.com>
2024-08-26 23:24:22 +0900

    chore: debugging 코드 추가

--- src/utils/fetchGraphData.js
+++ src/utils/fetchGraphData.js
@@ -1,6 +1,23 @@
 import axios from "axios";
 
-const fetchGraphData = async (apiUrl, endpoint, exampleData, setGraphData) => {
+axios.interceptors.request.use((request) => {
+  console.log("Starting Request", JSON.stringify(request, null, 2));
+  console.log("Request Headers:", JSON.stringify(request.headers, null, 2));
+  return request;
+});
+
+axios.interceptors.response.use((response) => {
+  console.log("Response:", JSON.stringify(response, null, 2));
+  return response;
+});
+
+const fetchGraphData = async (
+  token,
+  apiUrl,
+  endpoint,
+  exampleData,
+  setGraphData
+) => {
   if (import.meta.env.VITE_USE_MOCK_DATA === "true") {
     // Use example data
     const formattedNodes = exampleData.nodes.map((node) => ({
@@ -20,15 +37,18 @@ const fetchGraphData = async (apiUrl, endpoint, exampleData, setGraphData) => {
   } else {
     // Fetch data from API
     try {
-      // const token = import.meta.env.VITE_API_TOKEN;
-      const response = await axios.get(
-        apiUrl + endpoint
-        //   , {
-        //   headers: {
-        //     Authorization: `Bearer ${token}`,
-        //   },
-        // }
+      const response = await axios.get(apiUrl + endpoint, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      console.log("Response Status:", response.status);
+      console.log("Response Data:", JSON.stringify(response.data, null, 2));
+      console.log(
+        "Response Headers:",
+        JSON.stringify(response.headers, null, 2)
       );
+
       if (response.data.isSuccess) {
         const data = response.data.data;
         const formattedNodes = data.nodes.map((node) => ({
@@ -48,6 +68,17 @@ const fetchGraphData = async (apiUrl, endpoint, exampleData, setGraphData) => {
       }
     } catch (error) {
       console.error("Failed to fetch graph data:", error);
+      if (error.response) {
+        console.log("Error Response Status:", error.response.status);
+        console.log(
+          "Error Response Data:",
+          JSON.stringify(error.response.data, null, 2)
+        );
+        console.log(
+          "Error Response Headers:",
+          JSON.stringify(error.response.headers, null, 2)
+        );
+      }
     }
   }
 };
