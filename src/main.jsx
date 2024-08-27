fa5877015a7ad2bc4393a785c309478f0672ebe0
Dawon00 <dawon000203@gmail.com>
2024-08-27 23:51:53 +0900

    chore: 코드 정리

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,6 @@ import { Analytics } from "@vercel/analytics/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    {" "}
     <App />
     <Analytics />
   </BrowserRouter>
